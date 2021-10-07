96876b6d1e32d9854d398c2d295f0df1e3d62a77
mooy <61348580+mooyg@users.noreply.github.com>
2021-10-07 22:57:57 +0530

    fix: fixed all the maximum bugs with inserting emojis

--- packages/desktop/src/components/text-editor/Editor.tsx
+++ packages/desktop/src/components/text-editor/Editor.tsx
@@ -4,18 +4,8 @@ import {
   createPlateOptions,
   PlatePlugin,
   ELEMENT_IMAGE,
-  PlateProps,
-  createImagePlugin,
-  getVoidTypes,
-  useEventEditorId,
-  useStoreEditorRef,
-  getPlatePluginTypes,
-  getPlatePluginType,
   ELEMENT_DEFAULT,
-  getPointNextToVoid,
-  getPointFromLocation,
 } from '@udecode/plate'
-import { Location, Editor, Point } from 'slate'
 import { useStore } from '../../store/store'
 import { Emoticon } from '../emojis/Emoticon'
 import { DefaultElement } from './DefaultElement'
@@ -23,7 +13,6 @@ import { pluginsBasic } from './lib'
 import { BallonToolbarMarks } from './Options'
 
 export const ContentEditor = () => {
-  const editor = useStoreEditorRef(useEventEditorId('focus'))
   const { setShowEmojiPicker, setNavigationKeyPressed } = useStore()
   const createOnKeyDownPlugin = (): PlatePlugin => {
     return {
@@ -31,6 +20,7 @@ export const ContentEditor = () => {
         if (event.ctrlKey && event.code === 'Space') {
           setShowEmojiPicker(true)
         }
+        // TODO: Not use state here and use some other logic
         if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
           setNavigationKeyPressed(event.key)
         }
